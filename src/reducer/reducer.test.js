56879333bba17072ad7462565afe3055dcfd4a34
CW永松 <nagamatsucw@gmail.com>
2020-12-09 16:47:50 +0900

    reducer counter test

---
+++ src/reducer/reducer.test.js
@@ -0,0 +1,15 @@
+import reducer, { initialCounterState as initialState } from './counter'
+
+describe('reducer', () => {
+  // initialStateがそのまま返ってくるか
+
+  it("initialState test", () => {
+    expect(reducer(undefined,{})).toEqual(initialState);
+  })
+  test('increment', () => {
+    expect(reducer(initialState, { type: 'INCREMENT' })).toEqual({ count: 1})
+  })
+  test('increment', () => {
+    expect(reducer(initialState, { type: 'DECREMENT' })).toEqual({ count: -1})
+  })
+})
